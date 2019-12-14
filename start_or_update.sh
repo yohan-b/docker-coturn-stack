d7d49caeff78fc2a98190dc650ebefd68a579d5d
yohan <783b8c87@scimetis.net>
2019-12-14 23:22:13 +0100

    Launch script now retrieve secrets from encrypted secret archive.

--- start_or_update.sh
+++ start_or_update.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+test -z ${KEY} && { echo "KEY variable is not defined."; exit 1; }
+test -f ~/secrets.tar.gz.enc || curl -o ~/secrets.tar.gz.enc "https://cloud.scimetis.net/s/${KEY}/download?path=%2F&files=secrets.tar.gz.enc"
+openssl enc -aes-256-cbc -d -in ~/secrets.tar.gz.enc | tar -zxv --strip 2 secrets/docker-coturn-stack/secret
+sudo chown root. secret
+sudo chmod a-r secret
+sudo mv -f secret .env
+
 unset VERSION_COTURN
 VERSION_COTURN=$(git ls-remote https://git.scimetis.net/yohan/docker-coturn.git| head -1 | cut -f 1|cut -c -10) \
  sudo -E bash -c 'docker-compose up -d'
